1d1bd0420addeb5a47f38abfa447168f9d6eb8b4
konstantinvoronenko <undess222@gmail.com>
2021-05-03 14:12:45 +0300

    react

--- VOWELS_ARR/script.js
+++ VOWELS_ARR/script.js
@@ -1,5 +1,4 @@
 const countVowels = str => Array.from(str)
     .filter(letter => 'ауоыиэяюёе'.includes(letter)).length;
 
-
 console.log(countVowels('хеллоу'));
--- test/asd.js
+++ test/asd.js
@@ -1,6 +1,12 @@
-let arr = [1, 2, 3];
+const arr = [ {name: 'Bart'}, {name: 'Lisa'}, {name: 'Maggie'} ];
 
-let n = arr.map((el, index) => {
-    return 'Элемент: ' + el + ' индекс ' + index;
-})
-console.log(n)
+
+function list(names){
+    let result = [];
+    for (let i = 0; i < names.length - 1; i++) {
+        result.push(names[i].name + ', ');
+    }
+    return result.join('');
+}
+
+console.log(list(arr))
